4178af925daeff4e57de3459db8da98ab332fdfe
Konstantin Nazarov <kostr2010@gmail.com>
2022-05-13 00:43:46 +0300

    Ilya's save

--- emulator.h
+++ emulator.h
@@ -5,10 +5,60 @@
 #include "runtime/interpreter.h"
 #include "utils/ins.h"
 
-class emulator
+class Emulator
 {
   public:
   private:
+    Interpreter interpreter_;
+    std::vector<Ins> program_;
+    pmemory pmem_; // array with data
+    vmemory vmem_; // address array without data
+
+    Memory pmem_adr API(vmem_adr)
 };
+
+class vmem
+{
+    std::array<vpage_> addrs_;
+    void vread(int32_t vaddr)
+    {
+        is_vaddr_in_dynamic_programm_space();
+        pread(convert_to_physical(vaddr));
+    }
+};
+
+class pmem
+{};
+
+class vpage
+{
+    uint32_t addr_begin_region_;
+    uint32_t addr_end_regiob_;
+    atttibutes attrs_;
+};
+
+class ppage
+{};
+
+class KUSOK_PAMYATI
+{
+
+    GetKernelSpaceBegin();
+    GetKernelSpaceEnd();
+    CheckIfInKernelSpace();
+
+    GetStackBegin();
+    GetStackEnd();
+    CheckIfInStack();
+
+    data Read(uint32_t v_adr) void Write(uint32_t v_adr, data)
+
+        private : void* GetPAdr(uint32_t v_adr)
+    {
+        assert(v_adr <)
+    }
+
+    void* p_adr_space; // 4 mb
+}
 
 #endif
--- mem/mem.h
+++ mem/mem.h
@@ -1,4 +1,43 @@
 #ifndef MEM_H_INCLUDED
 #define MEM_H_INCLUDED
 
+// CSR and 32 basic registers are held in regfile
+
+// we have 34 bit physical address space and 32 bit virtual address space
+// simulator has 4mb of RAM
+
+// physical memory layout
+/*---------------------------------- 0x00000000
+|   imitation of restricted zone
+|----------------------------------- 0x0000000F
+|               Text (ELF)
+|-----------------------------------
+|               Global data
+|-----------------------------------
+|            stack (grows down)
+|                  |
+|-----------------------------------
+|                  |
+|            Heap (grows up)
+|----------------------------------- 0x003FFFFF
+*/
+
+// virtual memory layout
+/*---------------------------------- 0x00000000
+|       imitation of kernal space
+|----------------------------------- 0x000000FF
+|               Text (ELF)
+|-----------------------------------
+|               Global data
+|-----------------------------------
+|            stack (grows down)
+|                  |
+|-----------------------------------
+|                  |
+|            Heap (grows up)
+|----------------------------------- 0x003FFFFF
+*/
+
+// translation rule: addr[p] = addr[v] - 0x000000F0
+
 #endif
