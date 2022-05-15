5f7715aede70dc3065bebd2255e2ec188b0df680
Max <m.max.2000@mail.ru>
2022-05-15 17:40:01 +0300

    added class to read elf files

--- utils/readelf.cpp
+++ utils/readelf.cpp
@@ -1,21 +1,23 @@
+#include "readelf.h"
 #include <cstring>
 #include <sys/mman.h>
 #include <unistd.h>
-#include "readelf.h"
 
-bool ReadElf::ValidateElfHeader(const Elf32_Ehdr &elf_header) {
+bool ReadElf::ValidateElfHeader(const Elf32_Ehdr& elf_header)
+{
     int cmp = memcmp(&elf_header.e_ident,
                      "\x7f"
                      "ELF",
                      4);
-    return (cmp == 0)
-           && (elf_header.e_type == ET_EXEC)
-           && (elf_header.e_machine == EM_RISCV);
+    return (cmp == 0) && (elf_header.e_type == ET_EXEC) &&
+           (elf_header.e_machine == EM_RISCV);
 }
 
-const Elf32_Ehdr &ReadElf::ReadElfHeader() {
+const Elf32_Ehdr& ReadElf::ReadElfHeader()
+{
     if (!header_was_init) {
-        if (pread(fd_, &elf_header, sizeof(elf_header), 0) != sizeof(elf_header)) {
+        if (pread(fd_, &elf_header, sizeof(elf_header), 0) !=
+            sizeof(elf_header)) {
             throw ElfLoadException("failed to read ELF header");
         }
         if (!ValidateElfHeader()) {
@@ -26,11 +28,13 @@ const Elf32_Ehdr &ReadElf::ReadElfHeader() {
     return elf_header;
 }
 
-const std::vector<Elf32_Phdr> &ReadElf::ReadProgramHeaderTable() {
+const std::vector<Elf32_Phdr>& ReadElf::ReadProgramHeaderTable()
+{
     if (!table_was_init) {
         ph_table.resize(elf_header.e_phnum);
         long int table_bytes_size = sizeof(Elf32_Phdr) * elf_header.e_phnum;
-        if (pread(fd_, ph_table.data(), table_bytes_size, elf_header.e_phoff) != table_bytes_size) {
+        if (pread(fd_, ph_table.data(), table_bytes_size,
+                  elf_header.e_phoff) != table_bytes_size) {
             throw ElfLoadException("failed to load Program Header Table");
         }
         table_was_init = true;
@@ -38,11 +42,13 @@ const std::vector<Elf32_Phdr> &ReadElf::ReadProgramHeaderTable() {
     return ph_table;
 }
 
-uint32_t ReadElf::LoadElf32IntoMemory() {
+uint32_t ReadElf::LoadElf32IntoMemory()
+{
     ReadElfHeader();
     ReadProgramHeaderTable();
-    for (const auto &p_header : ph_table) {
-        if (p_header.p_type != PT_LOAD && p_header.p_memsz == 0) {   // must be loadable
+    for (const auto& p_header : ph_table) {
+        if (p_header.p_type != PT_LOAD &&
+            p_header.p_memsz == 0) { // must be loadable
             continue;
         }
 
@@ -60,7 +66,7 @@ uint32_t ReadElf::LoadElf32IntoMemory() {
         auto vaddr = p_header.p_vaddr;
         auto mem_size = p_header.p_memsz;
         auto file_size = p_header.p_filesz;
-        auto *paddr = AllocateMemory(vaddr, mem_size, prot);
+        auto* paddr = AllocateMemory(vaddr, mem_size, prot);
 
         if (pread(fd_, paddr, file_size, p_header.p_offset) != file_size) {
             throw ElfLoadException("failed to copy segment into memory");
@@ -74,43 +80,49 @@ uint32_t ReadElf::LoadElf32IntoMemory() {
 
 // TODO mmap munmap
 // First page allocation and cheking of other pages available
-void ReadElf::InitMemory() {
-    memory_ = reinterpret_cast<uint8_t*>(mmap(NULL,             // Need our own realization of mmap
-                                              Size,             // Need to choose our own max Size to mmap
-                                              PROT_NONE,
-                                              MAP_PRIVATE | MAP_ANONYMOUS,
-                                              -1, 0));
+void ReadElf::InitMemory()
+{
+    memory_ = reinterpret_cast<uint8_t*>(
+        mmap(NULL, // Need our own realization of mmap
+             Size, // Need to choose our own max Size to mmap
+             PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
 
     if (memory_ == MAP_FAILED) {
         throw ElfLoadException("Failed to init memory");
     }
-    if (munmap(memory_ + PageSize, Size - PageSize)) {          // Need our own realization of munmap
-        throw ElfLoadException("Failed to init memory");        // Checked that enough memory is available
-    }                                                           // Leave only one page of memory to save base ptr
+    if (munmap(memory_ + PageSize,
+               Size - PageSize)) { // Need our own realization of munmap
+        throw ElfLoadException("Failed to init memory"); // Checked that enough
+                                                         // memory is available
+    } // Leave only one page of memory to save base ptr
 }
 
-uint32_t ReadElf::AlignUp(uint32_t vaddr) {
+uint32_t ReadElf::AlignUp(uint32_t vaddr)
+{
     return ((vaddr + (PageSize - 1)) / PageSize) * PageSize;
 }
 
 // Get the begginning of page
-uint32_t ReadElf::AlignDown(uint32_t vaddr){
+uint32_t ReadElf::AlignDown(uint32_t vaddr)
+{
     return vaddr - vaddr % PageSize;
 }
 
 // TODO mmap
-uint8_t *ReadElf::AllocateMemory(uint32_t vaddr, size_t length, int prot) {
-    auto aligned_vaddr = AlignDown(vaddr);                                  // addr in mmap % 4096 == 0 else fault
-    length = AlignUp(length + vaddr - aligned_vaddr);                       //  len in mmap % 4096 == 0 else fault
+uint8_t* ReadElf::AllocateMemory(uint32_t vaddr, size_t length, int prot)
+{
+    auto aligned_vaddr =
+        AlignDown(vaddr); // addr in mmap % 4096 == 0 else fault
+    length = AlignUp(length + vaddr -
+                     aligned_vaddr); //  len in mmap % 4096 == 0 else fault
     if (Size - length < vaddr) {
-        throw ElfLoadException("failed to allocate " +
-                                    std::to_string(length) +
-                                    " bytes starting at " +
-                                    std::to_string(vaddr));
+        throw ElfLoadException("failed to allocate " + std::to_string(length) +
+                               " bytes starting at " + std::to_string(vaddr));
     }
 
-    void *paddr = mmap(Translate(aligned_vaddr), length, prot | PROT_WRITE, // Need our own realization of mmap
-                        MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    void* paddr = mmap(Translate(aligned_vaddr), length,
+                       prot | PROT_WRITE, // Need our own realization of mmap
+                       MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (paddr == MAP_FAILED) {
         throw ElfLoadException("failed to allocate memory");
     }
@@ -120,10 +132,8 @@ uint8_t *ReadElf::AllocateMemory(uint32_t vaddr, size_t length, int prot) {
 }
 
 // TODO memset
-void *ReadElf::Memset(uint32_t vaddr, int fill_byte, size_t n_bytes) {
-    return memset(Translate(vaddr), fill_byte, n_bytes);                    // Need our own realization of memset
-}
-
-void ReadElf::SetFD(int fd) {
-    fd_ = fd;
+void* ReadElf::Memset(uint32_t vaddr, int fill_byte, size_t n_bytes)
+{
+    return memset(Translate(vaddr), fill_byte,
+                  n_bytes); // Need our own realization of memset
 }
--- utils/readelf.h
+++ utils/readelf.h
@@ -8,44 +8,47 @@
 #include <unistd.h>
 #include <vector>
 
-using Pointer=uint8_t *;
+using Pointer = uint8_t*;
 
-class ElfLoadException : public std::runtime_error {
-public:
-    explicit ElfLoadException(const std::string& mes) : std::runtime_error(mes) {};
-    explicit ElfLoadException(const char *mes) : std::runtime_error(mes) {};
+class ElfLoadException : public std::runtime_error
+{
+  public:
+    explicit ElfLoadException(const std::string& mes)
+        : std::runtime_error(mes){};
+    explicit ElfLoadException(const char* mes) : std::runtime_error(mes){};
 };
 
-class ReadElf{
-public:
+class ReadElf
+{
+  public:
+    explicit ReadElf()
+        : fd_(-1), elf_header{}, header_was_init(false), table_was_init(false)
+    {
+    }
 
-    explicit ReadElf(): fd_(-1),
-                        elf_header{},
-                        header_was_init(false),
-                        table_was_init(false){}
+    explicit ReadElf(int fd)
+        : fd_(fd), elf_header{}, header_was_init(false), table_was_init(false)
+    {
+    }
 
-    explicit ReadElf(int fd): fd_(fd),
-                              elf_header{},
-                              header_was_init(false),
-                              table_was_init(false){}
-
-    // TODO munmap
-    ~ReadElf() {
-        for (const auto &it : allocated_chunks_) {
-            if (munmap(it.first, it.second)) {  // Need our own realization of munmap
+    ~ReadElf()
+    {
+        for (const auto& it : allocated_chunks_) {
+            if (munmap(it.first, it.second)) {
             }
         }
         allocated_chunks_.clear();
-        if (munmap(memory_, PageSize)) {        // Need our own realization of munmap
+        if (munmap(memory_, PageSize)) {
         }
     }
 
-    static bool ValidateElfHeader(const Elf32_Ehdr &elf_header);
-    bool ValidateElfHeader() const {
+    static bool ValidateElfHeader(const Elf32_Ehdr& elf_header);
+    bool ValidateElfHeader() const
+    {
         return ValidateElfHeader(elf_header);
     }
-    const Elf32_Ehdr &ReadElfHeader();
-    const std::vector<Elf32_Phdr> &ReadProgramHeaderTable();
+    const Elf32_Ehdr& ReadElfHeader();
+    const std::vector<Elf32_Phdr>& ReadProgramHeaderTable();
     uint32_t LoadElf32IntoMemory();
     void InitMemory();
 
@@ -54,24 +57,23 @@ public:
 
     uint32_t AlignUp(uint32_t vaddr);
     uint32_t AlignDown(uint32_t vaddr);
-    uint8_t *AllocateMemory(uint32_t vaddr, size_t length, int prot);
-    void *Memset(uint32_t vaddr, int fill_byte, size_t n_bytes);
-    void SetFD(int fd);
+    uint8_t* AllocateMemory(uint32_t vaddr, size_t length, int prot);
+    void* Memset(uint32_t vaddr, int fill_byte, size_t n_bytes);
 
-    inline uint8_t *Translate(uint32_t vaddr) const {
-        return memory_ + vaddr;
-    }
-
-private:
+  private:
     int fd_;
     Elf32_Ehdr elf_header;
     bool header_was_init;
     bool table_was_init;
     std::vector<Elf32_Phdr> ph_table;
-    //TODO
+    // TODO
     Pointer memory_; // Realize pointer
-    std::vector<std::pair<void*, size_t>> allocated_chunks_;
-};
+    std::vector<std::pair<void*, size_t> > allocated_chunks_;
 
+    inline uint8_t* Translate(uint32_t vaddr) const
+    {
+        return memory_ + vaddr;
+    }
+};
 
 #endif // UTILS_READELF_H_INCLUDED
